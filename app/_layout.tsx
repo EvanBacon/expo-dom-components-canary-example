94fe095ac0c32257f3fc156cd0c3255cb37b9a63
evanbacon <bacon@expo.io>
2024-08-15 14:37:57 -0500

    adjusted web scrolling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
-import { Tabs } from "expo-router";
+import { Slot, Tabs } from "expo-router";
 
 export default function RootLayout() {
+  if (process.env.EXPO_OS === "web") return <Slot />;
+
   return (
     <Tabs>
       <Tabs.Screen name="index" options={{ title: "shadcn" }} />
-      <Tabs.Screen name="second" options={{ title: "MDX" }} />
+      <Tabs.Screen name="products" options={{ title: "MDX" }} />
     </Tabs>
   );
 }
