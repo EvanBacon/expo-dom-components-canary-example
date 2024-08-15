188b473cf63d8538c13582601c6ae37bdf9ee001
evanbacon <bacon@expo.io>
2024-08-15 12:01:03 -0500

    running in expo go ios

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import DomView from '@/components/DomView';
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1 }}>
+      <DomView />
+    </View>
+  );
+}
---
+++ components/DomView.tsx
@@ -0,0 +1,9 @@
+'use dom';
+
+export default function DomView() {
+  return (
+    <svg width="200" height="200">
+      <circle cx="100" cy="100" r="80" fill="blue" />
+    </svg>
+  );
+}
