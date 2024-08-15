3035eecdf83c9e0ba35d3a15c81da24766a20c0b
evanbacon <bacon@expo.io>
2024-08-15 14:15:49 -0500

    add some charts

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -75,6 +75,8 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
+import BasicChart from "./basic-chart";
+
 // @ts-expect-error
 const IS_DOM = typeof ReactNativeWebView !== "undefined";
 
@@ -305,6 +307,7 @@ export default function Dashboard({
           <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
             <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
               <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
+                <BasicChart />
                 <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
                   <CardHeader className="pb-3">
                     <CardTitle>Your Orders</CardTitle>
