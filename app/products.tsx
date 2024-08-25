2576c74ede06c050d26b306fe42d03afd6005ad0
evanbacon <bacon@expo.io>
2024-08-25 14:36:13 +0200

    update markdown example

--- app/products.tsx
+++ app/products.tsx
@@ -5,7 +5,6 @@ import "@/global.css";
 import Story from "@/components/mdx/story.mdx";
 import { getDOMComponents, MDXComponents } from "@bacons/mdx";
 import { Highlight, themes } from "prism-react-renderer";
-
 const webElements = getDOMComponents();
 
 export default function StoryWrapper() {
@@ -13,7 +12,7 @@ export default function StoryWrapper() {
   const Img = webElements.img;
   // Provide pure DOM elements for the MDX.
   return (
-    <div className="p-4 max-w-[100vw]">
+    <div className="p-4 max-w-[100vw] container mx-auto px-4 md:px-6 lg:px-0 max-w-3xl">
       <MDXComponents
         components={{
           // Ensure we use default DOM elements instead of the default universal elements.
@@ -22,11 +21,12 @@ export default function StoryWrapper() {
           // Good tailwind defaults for MDX.
           img: (props) => <Img {...props} className="max-w-full rounded-md" />,
           h1: (props) => <h1 {...props} className="text-4xl font-bold mb-4" />,
+          h2: (props) => <h2 {...props} className="text-3xl font-bold mb-4" />,
           hr: (props) => <hr {...props} className="my-4 border-gray-300" />,
           p: (props) => <p {...props} className="mb-4" />,
           ul: (props) => <ul {...props} className="list-disc pl-4" />,
           ol: (props) => <ol {...props} className="list-decimal pl-4" />,
-          li: (props) => <li {...props} className="mb-2" />,
+          li: ({ parentName, ...props }) => <li {...props} className="mb-2" />,
           a: (props) => <a {...props} className="text-blue-500" />,
           blockquote: (props) => (
             <blockquote
@@ -41,6 +41,18 @@ export default function StoryWrapper() {
             />
           ),
           code: CustomCode,
+          table: (props) => (
+            <table
+              {...props}
+              className="table-auto w-full border-collapse border border-gray-300"
+            />
+          ),
+          th: ({ parentName, ...props }) => (
+            <th {...props} className="border border-gray-300 p-2" />
+          ),
+          td: ({ parentName, ...props }) => (
+            <td {...props} className="border border-gray-300 p-2" />
+          ),
         }}
       >
         <Story />
--- app/react-three-fiber.tsx
+++ app/react-three-fiber.tsx
@@ -3,6 +3,9 @@ import RTFShoes from "@/components/shoes";
 export default function App() {
   return (
     <RTFShoes
+      style={{
+        height: "100vh",
+      }}
       dom={{
         scrollEnabled: false,
       }}
