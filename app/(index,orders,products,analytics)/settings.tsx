74ed490dc5e7feca7a7f1824180bbad739405d37
evanbacon <bacon@expo.io>
2024-08-30 22:55:37 +0200

    Update settings.tsx

--- app/(index,orders,products,analytics)/settings.tsx
+++ app/(index,orders,products,analytics)/settings.tsx
@@ -1,15 +1,19 @@
 import SettingsRoute from "@/components/shad/settings";
 import { router } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function Settings() {
   return (
-    <SettingsRoute
-      navigate={router.navigate}
-      dom={{
-        contentInsetAdjustmentBehavior: "automatic",
-        automaticallyAdjustsScrollIndicatorInsets: true,
-      }}
-    />
+    <>
+      <SettingsRoute
+        navigate={router.navigate}
+        dom={{
+          contentInsetAdjustmentBehavior: "automatic",
+          automaticallyAdjustsScrollIndicatorInsets: true,
+        }}
+      />
+      <StatusBar barStyle={"light-content"} />
+    </>
   );
 }
 
