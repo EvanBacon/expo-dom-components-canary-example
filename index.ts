04501c8f39ddd9652147c300b7af2e6c1849e114
evanbacon <bacon@expo.io>
2024-09-02 19:31:53 +0200

    add quick actions

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import "expo-router/entry";
+import { Action, setItems } from "expo-quick-actions";
 
 import * as Notifications from "expo-notifications";
 
@@ -9,3 +10,32 @@ Notifications.setNotificationHandler({
     shouldSetBadge: false,
   }),
 });
+
+// Mock quick actions
+
+const items: Action[] = [
+  {
+    id: "0",
+    title: "View Orders",
+    subtitle: "3 New Orders",
+    icon: "symbol:mail.stack",
+  },
+  {
+    id: "3",
+    title: "Check out",
+    subtitle: "14 Items",
+    icon: "symbol:cart",
+  },
+  {
+    id: "1",
+    title: "Search",
+    icon: "search",
+  },
+  {
+    id: "2",
+    title: "View Profile",
+    icon: "symbol:person.2",
+  },
+];
+
+setItems(items);
