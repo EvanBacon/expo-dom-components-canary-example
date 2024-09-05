bc0e94952632bae8136fa57be254257f42ba99af
evanbacon <bacon@expo.io>
2024-09-05 12:30:23 +0200

    use next version of atlas

--- index.ts
+++ index.ts
@@ -3,7 +3,11 @@ import "expo-router/entry";
 import { LogBox } from "react-native";
 import * as Notifications from "expo-notifications";
 
-LogBox.ignoreLogs(["Failed to attach search bar to the header"]);
+LogBox.ignoreLogs([
+  "Failed to attach search bar to the header",
+  // Shows when adding the tab bar during fast refresh. Maybe related to the scrolling system?
+  "Error evaluating injectedJavaScript:",
+]);
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
