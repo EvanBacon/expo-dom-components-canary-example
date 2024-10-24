723c38c994bbe69a2b25c9f85d2e913c98dbdcfc
evanbacon <bacon@expo.io>
2024-10-24 14:31:47 -0500

    upgrade to latest canary

--- index.ts
+++ index.ts
@@ -4,7 +4,8 @@ import { LogBox } from "react-native";
 import * as Notifications from "expo-notifications";
 
 LogBox.ignoreLogs([
-  "Failed to attach search bar to the header",
+  "functionality provided by expo-notifications",
+  "functionality is not fully supported in Expo Go",
   // Shows when adding the tab bar during fast refresh. Maybe related to the scrolling system?
   "Error evaluating injectedJavaScript:",
 ]);
